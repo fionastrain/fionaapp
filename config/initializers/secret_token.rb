78167846ef6729cba70fbc8dfcf8c757cb9647fa
fionastrain <fiona.strain1@gmail.com>
2013-01-15 17:07:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fionaapp::Application.config.secret_token = '6c3b168f595c044251a8eb617d5211636659e9442139e44c224e52985832c2e5103a67932bd6310ba4dfa91f9eb369367634dda8861deda19a03b21ce8ac080d'
